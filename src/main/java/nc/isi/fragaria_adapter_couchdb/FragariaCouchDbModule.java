eed95e9fde2148405a66b2a40d748d6428d0de86
Justin Maltat <jmaltat@isi.nc>
2013-11-12 09:59:18 +1100

    Suppression de la dépendance au FragariaDomainModule pour permettre à l'application de choisir entre les sessions simples et celles avec ElasticSearch

--- src/main/java/nc/isi/fragaria_adapter_couchdb/FragariaCouchDbModule.java
+++ src/main/java/nc/isi/fragaria_adapter_couchdb/FragariaCouchDbModule.java
@@ -3,15 +3,12 @@ package nc.isi.fragaria_adapter_couchdb;
 import nc.isi.fragaria_adapter_couchdb.views.CouchDbViewConfigBuilder;
 import nc.isi.fragaria_adapter_rewrite.dao.adapters.Adapter;
 import nc.isi.fragaria_adapter_rewrite.entities.views.ViewConfigBuilder;
-import nc.isi.fragaria_adapter_rewrite.services.FragariaDomainModule;
 
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
-import org.apache.tapestry5.ioc.annotations.SubModule;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
-@SubModule(FragariaDomainModule.class)
 public class FragariaCouchDbModule {
 	static {
 		SLF4JBridgeHandler.install();
